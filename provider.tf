d27c32f81101acd98b9bfba431b35f7b7b2c5285
vinayakmet <vinayak@searchstax.com>
2026-06-24 10:31:36 +0530

    terraform data added

--- provider.tf
+++ provider.tf
@@ -1,1 +1,22 @@
+terraform {
+     backend "azurerm" {
+      resource_group_name  = "mygithubaction-rg"
+      storage_account_name = "mygithubactiontfstate"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+    
+    }
+  }
 
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" 
+  features {}
+}
